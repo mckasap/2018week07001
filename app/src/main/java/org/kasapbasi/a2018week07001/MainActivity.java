2cef0b86565c297994f2b3f772f9b974f314dbcb
mckasap <mckasap@gmail.com>
2018-03-29 10:17:21 +0300

    onClick eklendi

--- app/src/main/java/org/kasapbasi/a2018week07001/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week07001/MainActivity.java
@@ -1,8 +1,12 @@
 package org.kasapbasi.a2018week07001;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void CheckPermission(View view) {
+
+        int PermissionCheck= ContextCompat.checkSelfPermission (this,"Manifest.permission.READ_CALENDAR");
+        if( PermissionCheck!= PackageManager.PERMISSION_GRANTED){
+
+            if(shouldShowRequestPermissionRationale("Manifest.permission.READ_CALENDAR")){
+
+
+
+            }else {
+                requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, 0);
+            }
+
+
+        }else
+        {
+
+            Toast.makeText(this,"ZATEN iznin Var",Toast.LENGTH_LONG).show();
+        }
 
 
     }
