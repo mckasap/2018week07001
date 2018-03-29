8a7ddbbd41ae8102d0e8acecc190f5edf9c4adce
mckasap <mckasap@gmail.com>
2018-03-29 10:35:23 +0300

    onClick eklendi

--- app/src/main/java/org/kasapbasi/a2018week07001/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week07001/MainActivity.java
@@ -2,18 +2,63 @@ package org.kasapbasi.a2018week07001;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+     final int readCalender=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        switch (requestCode){
+
+            case readCalender:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this,"İzni Kaptım",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(this,"Tekrar İzni iste",Toast.LENGTH_LONG).show();
+
+
+
+                break;
+            default:
+
+        }
+
     }
 
     public void CheckPermission(View view) {
@@ -26,9 +71,8 @@ public class MainActivity extends AppCompatActivity {
 
 
             }else {
-                requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, 0);
+                requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, readCalender);
             }
-
 
         }else
         {
